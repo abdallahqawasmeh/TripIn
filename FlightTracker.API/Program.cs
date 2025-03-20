f16c4932ddeefdfea981bde6664d5ae65bb025a8
abdallahqawasmeh <abdallahqawasmeh132@gmail.com>
2025-03-20 06:04:02 +0300

    enter

---
+++ FlightTracker.API/Program.cs
@@ -0,0 +1,86 @@
+using FlightTracker.Core.Common;
+using FlightTracker.Core.Repository;
+using FlightTracker.Core.Service;
+using FlightTracker.Infra.Common;
+using FlightTracker.Infra.Repository;
+using FlightTracker.Infra.Service;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<IDbContext, DbContext>();
+builder.Services.AddScoped<IAboutUsRepository, AboutUsRepository>();
+builder.Services.AddScoped<IContactUsRepository, ContactUsRepository>();
+builder.Services.AddScoped<IContactInfoRepository, ContactInfoRepository>();
+builder.Services.AddScoped<ICompanyRepository, CompanyRepository>();
+builder.Services.AddScoped<IInvoiceRepository, InvoiceRepository>();
+builder.Services.AddScoped<IAdminRepository, AdminRepository>();
+builder.Services.AddScoped<IAirportRepository, AirportRepository>();
+builder.Services.AddScoped<IFlightRepository, FlightRepository>();
+
+
+builder.Services.AddScoped<IAirportService,AirportService>();
+builder.Services.AddScoped<ICompanyService,CompanyService >();
+
+builder.Services.AddScoped<IFlightService, FlightService>();
+
+
+
+
+
+
+
+
+
+builder.Services.AddAuthentication(opt => {
+
+	opt.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+
+	opt.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+
+}).AddJwtBearer(options =>
+
+{
+
+	options.TokenValidationParameters = new TokenValidationParameters
+
+	{
+
+		ValidateIssuer = false,
+
+		ValidateAudience = false,
+
+		ValidateLifetime = true,
+
+		ValidateIssuerSigningKey = true,
+
+		IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("superSecretKeyDana@345"))
+
+	};
+
+});
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+	app.UseSwagger();
+	app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
